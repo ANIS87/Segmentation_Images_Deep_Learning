e9d284be69dddc719dae5bec692a913af38dbf4f
KRAYNI ANIS <anis.krayni@gmail.com>
2017-01-24 11:41:01 +0100

    Update test_mask.py

--- Test/test_mask.py
+++ Test/test_mask.py
@@ -2,8 +2,8 @@
 from PIL import Image
 import numpy as np
 #[ 0  2  3  4  5 11 15 16]
-mask = np.array(Image.open('segmentation.png'))
-img = np.array(Image.open('dataset10k_1400.jpg'))
+mask = np.array(Image.open('a.png'))
+img = np.array(Image.open('a.jpg'))
 chk = np.ones(img.shape,dtype=img.dtype)
 print(img.shape)
 print(np.unique(mask))
@@ -16,24 +16,4 @@ print(mask.shape)
 print(res.shape)
 result = Image.fromarray(res)
 result.show()
-result.save('final.jpg')
-
-'''
-x =res *255
-#plt.imshow(x, cmap='gray', interpolation='nearest', vmin=0, vmax=255)
-
-  # Creates PIL image
-img = Image.fromarray(x, 'L')
-  #img.show()
-  #print(np.unique(array_image))
-  #Rescale to 0-255 and convert to uint8
-data=x
-rescaled = (255.0*data).astype(np.uint8)
-  
-im = Image.fromarray(rescaled)
-save_image='final.png'
-im.save(save_image)
-  #im = Image.open('test.png')
-  #in_ = np.array(im).astype('uint8')
-  #print(np.unique(in_))
-'''
+result.save('image_with_segmentation.jpg')
