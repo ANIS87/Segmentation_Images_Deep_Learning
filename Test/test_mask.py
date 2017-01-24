b3f12ebb9577068460d6bb0db72e6d994f280445
KRAYNI ANIS <anis.krayni@gmail.com>
2017-01-24 11:43:39 +0100

    Update test_mask.py

--- Test/test_mask.py
+++ Test/test_mask.py
@@ -1,15 +1,14 @@
 
 from PIL import Image
 import numpy as np
-#[ 0  2  3  4  5 11 15 16]
 mask = np.array(Image.open('a.png'))
 img = np.array(Image.open('a.jpg'))
+all_class=np.unique(mask)
+random_class=int(random.choice(all_class))#example: Shoes (16)
 chk = np.ones(img.shape,dtype=img.dtype)
-print(img.shape)
-print(np.unique(mask))
 # all the work done in these two lines
 mask = mask[:,:,np.newaxis]
-res = np.where(mask==2, chk, img)
+res = np.where(mask==random_class, chk, img)
 res=np.array(res,dtype=img.dtype)
 print(img.shape)
 print(mask.shape)
